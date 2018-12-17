8a023259e12e1f06b67130429eb45dc9c1b0f93b
patrickconnors23 <patconnors23@gmail.com>
2018-12-17 18:45:42 -0500

    my floor how caught

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import os, subprocess, random
 
 def generateFileText():
+    """
+    Generate random text for file
+    """
     path = "words.txt"
     with open("words.txt", "r") as f:
         words = [line for line in f.readlines()]
-    # numWords = random.randint(6, 14) 
     text = [words[random.randint(0, len(words) -1)].strip() for _ in range(random.randint(6, 14))]
 
     return " ".join(text)+"\r\n"
 
 def writeToFile():
+    """
+    Write to content.txt so that there is change to commit
+    """
     text = generateFileText() 
     with open("content.txt", "a+") as f:
         f.write(text)
     return text
 
 def generateCommitMessage(text):
-    return text
+    """
+    Shorten file text to generate commit message"
+    """
+    text = text.split()[:4]
+    return " ".join(text)
 
 def gitCommand(args):
+    """
+    Wrapper to execute git command via python
+    """
     GIT = "/usr/local/bin/git"
     subprocess.call([GIT]+args)
 
 def pushChange():
+    """
+    Write change and push to remote
+    """
     fileText = writeToFile()
     commitMessage = generateCommitMessage(fileText)
     gitCommand(["add", "."])
@@ -30,6 +45,10 @@ def pushChange():
     gitCommand(["push"]) 
 
 def main():
+    """
+    Randomly create change to push to github
+    This script should be run daily
+    """
     if random.random() < 0.75:
         for _ in range(random.randint(1, 5)):
             pushChange()
