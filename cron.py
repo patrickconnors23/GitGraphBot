1bb666d4e1121487e4f823d080056d5c95ff01d5
patrickconnors23 <patconnors23@gmail.com>
2018-12-21 16:47:00 -0500

    gave necessary help life

--- cron.py
+++ cron.py
@@ -4,11 +4,10 @@ from crontab import CronTab
 dirPath = os.path.dirname(os.path.abspath(__file__))
 
 python3 = subprocess.check_output(["which", "python3"]).decode('utf-8').strip()
-print(python3)
 
-# cron = CronTab(user='patrickconnors')
+cron = CronTab(user='patrickconnors')
 
-# job = cron.new(command=f"cd {dirPath}; {python3} main.py")  
-# job.hour.every(6)
+job = cron.new(command=f"cd {dirPath}; {python3} main.py")  
+job.minute.every(1)
 
-# cron.write()  
+cron.write()  
