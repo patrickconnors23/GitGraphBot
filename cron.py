f8a4a39a208ce7a4699048ccf1519932ec352f83
patrickconnors23 <patconnors23@gmail.com>
2018-12-23 17:14:13 -0500

    win own fit reach

--- cron.py
+++ cron.py
@@ -1,13 +1,19 @@
 import os, subprocess, sys
 from crontab import CronTab
 
+# Get current directory`
 dirPath = os.path.dirname(os.path.abspath(__file__))
 
+# Get version of python to run
 python3 = subprocess.check_output(["which", "python3"]).decode('utf-8').strip()
+git = subprocess.check_output(["which", "git"]).decode('utf-8').strip()
 
+# Init Crontab instance 
 cron = CronTab(user='patrickconnors')
 
-job = cron.new(command=f"cd {dirPath}; {python3} main.py")  
+# Add command
+job = cron.new(command=f"cd {dirPath}; {git} config --global -l")  
+# job = cron.new(command=f"cd {dirPath}; {python3} main.py")  
 job.minute.every(1)
 
 cron.write()  
