02052f6179d4fbc229a01adddd0e4cfd593301ed
patrickconnors23 <patconnors23@gmail.com>
2019-09-17 20:31:31 -0700

    remove native cron creation

--- cron.py
+++ cron.py
@@ -1,4 +1,6 @@
-import os, subprocess, sys
+import os
+import subprocess
+import sys
 from crontab import CronTab
 
 # Get current directory`
@@ -7,12 +9,12 @@ dirPath = os.path.dirname(os.path.abspath(__file__))
 # Get version of python to run
 python3 = subprocess.check_output(["which", "python3"]).decode('utf-8').strip()
 
-# Init Crontab instance 
+# Init Crontab instance
 cron = CronTab(user='patrickconnors')
 
 # Add command
-job = cron.new(command=f"cd {dirPath}; {python3} main.py")  
+job = cron.new(command=f"cd {dirPath}; {python3} main.py")
 job.minute.on(0)
 job.hour.on(20)
 
-cron.write()  
+cron.write()
